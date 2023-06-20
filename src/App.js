939e1685efc620451b2289d7cc2d7387835ee63a
Fardan Faturrahman <danfardan16@gmail.com>
2023-06-20 19:48:50 +0700

    react-example

---
+++ src/App.js
@@ -0,0 +1,42 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Belajar ReactJs</h1>
+      <div className="container">
+        <div className='row'>
+          <div className="items">
+            <img className='img-card' src='https://img.freepik.com/free-vector/flat-design-autumn-landscape-with-sun_23-2149154923.jpg'/>
+            <div className='body-card'>
+              <h1 className='title-card'>Ini ceritanya card title</h1>
+              <h2 className='subtitle-card'>Ini ceritanya card subtitle</h2>
+              <p className='desk-card'>Ini ceritanya card deskripsi</p>
+              <button className='btn'>Button</button>
+            </div>
+          </div>
+          <div className="items">
+            <img className='img-card' src='https://img.freepik.com/free-vector/flat-design-autumn-landscape-with-sun_23-2149154923.jpg'/>
+            <div className='body-card'>
+              <h1 className='title-card'>Ini ceritanya card title</h1>
+              <h2 className='subtitle-card'>Ini ceritanya card subtitle</h2>
+              <p className='desk-card'>Ini ceritanya card deskripsi</p>
+              <button className='btn'>Button</button>
+            </div>
+          </div>
+          <div className="items">
+            <img className='img-card' src='https://img.freepik.com/free-vector/flat-design-autumn-landscape-with-sun_23-2149154923.jpg'/>
+            <div className='body-card'>
+              <h1 className='title-card'>Ini ceritanya card title</h1>
+              <h2 className='subtitle-card'>Ini ceritanya card subtitle</h2>
+              <p className='desk-card'>Ini ceritanya card deskripsi</p>
+              <button className='btn'>Button</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
